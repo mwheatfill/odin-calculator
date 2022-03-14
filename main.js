c492a77bcff3637d08b270efb2f4d8fb7a8a53e6
Michael Wheatfill <michael@wheatfill.com>
2022-03-14 16:52:28 -0700

    core functionality nearly complete. Currently has bugs for several operations

--- main.js
+++ main.js
@@ -28,11 +28,75 @@ const operate = (operator, firstValue, secondValue) => {
     return subtract(firstValue, secondValue);
   }
 
-  if (operator == '*') {
+  if (operator == 'x') {
     return multiply(firstValue, secondValue);
   }
 
   if (operator == '/') {
     return divide(firstValue, secondValue);
   }
+};
+
+const updateDisplay = function (value) {
+  const display = document.querySelector('.display');
+  !value ? (display.textContent = value) : (display.textContent += value);
+};
+
+const clearExpression = function () {
+  expression.operator = '';
+  expression.leftValue = '';
+  expression.rightValue = '';
+};
+
+const updateValue = function (e) {
+  if (e.target.textContent == 'C') {
+    updateDisplay('');
+    clearExpression();
+    return;
+  }
+
+  if (!expression.operator) {
+    expression.leftValue += e.target.textContent;
+    updateDisplay(e.target.textContent);
+  }
+
+  if (expression.operator) {
+    expression.rightValue += e.target.textContent;
+    updateDisplay(e.target.textContent);
+  }
+  console.log(expression);
+};
+
+const updateOperator = function (e) {
+  if (!expression.leftValue) {
+    return;
+  }
+  if (!expression.operator || !expression.rightValue) {
+    expression.operator = e.target.textContent;
+    updateDisplay(expression.operator);
+  }
+
+  if (expression.rightValue) {
+    result = (operate(expression.operator, Number(expression.leftValue), Number(expression.rightValue)));
+    clearExpression();
+    expression.leftValue = result;
+    updateDisplay('');
+    updateDisplay(result);
+  }
+};
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+  number.addEventListener('click', updateValue);
+});
+
+const operations = document.querySelectorAll('.operation');
+operations.forEach((operation) => {
+  operation.addEventListener('click', updateOperator);
+});
+
+const expression = {
+  operator: '',
+  leftValue: '',
+  rightValue: '',
 };
